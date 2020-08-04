39de67530d90074955bd28d29e67d052eaaa0329
gucarletto <gucarletto@gmail.com>
2020-08-03 22:09:13 -0300

    Web Landing page

---
+++ web/src/pages/TeacherForm/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function TeacherForm() {
+  return (
+    <h2>List</h2>
+  );
+}
+
+export default TeacherForm;
---
+++ web/src/pages/TeacherList/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function TeacherList() {
+  return (
+    <h2>List</h2>
+  );
+}
+
+export default TeacherList;
