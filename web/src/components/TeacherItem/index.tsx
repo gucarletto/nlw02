39e309d046aee2c561224285c2537f181bbaf3fd
gucarletto <gucarletto@gmail.com>
2020-08-04 00:13:36 -0300

    Finished class one

---
+++ web/src/components/TeacherItem/index.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
+import './styles.css';
+
+const TeacherItem = () => {
+  return (
+    <main>
+      <article className="teacher-item" >
+        <header>
+          <img src="https://avatars2.githubusercontent.com/u/1106206?s=460&u=9416ae1c58cca0b8c42b9b9006b6bf41119bc9df&v=4" alt="Avatar"/>
+          <div>
+            <strong>Nome Professor</strong>
+            <span>Nada</span>
+          </div>
+        </header>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+          Eum voluptates obcaecati ipsam iste eos sit a odit maxime tempore quam? Suscipit atque pariatur ut temporibus minima saepe architecto placeat praesentium?
+        </p>
+        <footer>
+          <p>
+            Preço/Hora
+            <strong>R$5,00</strong>
+          </p>
+          <button type="button">
+            <img src={whatsappIcon} alt="Whats"/>
+            Entrar em contato
+          </button>
+        </footer>
+      </article>
+      </main>
+  );
+}
+
+export default TeacherItem;
--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import PageHeader from '../../components/PageHeader';
 
 function TeacherForm() {
   return (
-    <h2>List</h2>
+    <div id="page-teacher-form" className="container">
+      <PageHeader title="Que incível que você quer dar aulas" />
+    </div>
   );
 }
 
